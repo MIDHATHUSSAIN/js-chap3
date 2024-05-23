3bf9bf686eab2ed8f7047a716e37d56e0f28cc6d
MIDHAT-HUSSAIN <135623033+MIDHATHUSSAIN@users.noreply.github.com>
2024-05-23 13:54:53 +0500

    Add files via upload

---
+++ variablesForNumbers.js
@@ -0,0 +1,28 @@
+//Question: 1
+
+let age="15";
+
+alert("I am " + age + " years old");
+
+//Question: 2
+
+
+
+
+
+//Question: 3
+
+let birthYear="1990";
+
+document.write("My birth year is " + birthYear + "<br> Data type of my ddeclared variable is number");
+document.write("<br>");
+
+//Question: 4
+
+let visitorName="John Doe";
+let productTitle="T-Shirts"
+let quantity="5"
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing Sotre.");
+
+
